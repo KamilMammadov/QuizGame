b29165bab89597169061cf537fd57ceb64a8224c
KamilMammadov <kamiltm@code.edu.az>
2023-01-12 00:02:43 +0400

    initual commit

---
+++ QuizGame/Database/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuizGame.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options)
+         : base(options)
+        {
+
+        }
+    }
+}
